6fde5be5832c7701e26f6f17a899d6ed59993032
lishutong <lishutong@google.com>
2021-07-17 04:13:39 +0000

    Refactor the OTA configuration page using several components.
    The original code for OTA configuration in the frontend is tightly
    coupled and hard to maintain. This CL decoupled the job configuration
    component (OTAOptions.vue), build library (BuildLibrary.vue) and job
    submission (JobSubmission.js).
    
    The BuildLibrary.vue will present the
    target build list and take selections. Selections will be sent to
    OTAOptions.vue, where flags are set. Once the setting is complete, an
    OTAOption object defined by JobSubmission.js will be sent to the
    backend. This design can be viewed in go/ota-dashboard-doc : detailed
    design - TASK CONFIGURATION.
    
    This has several benefits: (a) easier for
    future maintainence. (b) The original code cannot easily be reused for
    batch OTA package generation. Now, only the OTAOptions.vue has to be
    modified for this purpose.
    
    Test: Mannual tested. Unit tests will be added in following CLs.
    
    Change-Id: I846d0c242cd6ed51478dd4d1a3e4c5fb8878aba1

---
+++ tools/otagui/src/services/JobSubmission.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Class OTAInput is used to configure and create a process in
+ * the backend to to start OTA package generation.
+ * @package vue-uuid
+ * @package ApiServices
+ */
+import { uuid } from 'vue-uuid'
+import ApiServices from './ApiService.js'
+
+export class OTAInput {
+  /**
+   * Initialize the input for the api /run/<id>
+   */
+  constructor() {
+    this.verbose = false,
+    this.target = '',
+    this.output = 'output/',
+    this.incremental = '',
+    this.isIncremental = false,
+    this.partial = '',
+    this.isPartial = false,
+    this.extra = '',
+    this.id = uuid.v1()
+    this.output += String(this.id) + '.zip'
+  }
+
+  /**
+   * Start the generation process, will throw an error if not succeed
+   */
+  async sendForm() {
+    try {
+      let response = await ApiServices.postInput(JSON.stringify(this), this.id)
+      return response.data
+    } catch (err) {
+      throw err
+    }
+  }
+}
---
+++ tools/otagui/src/views/JobConfigure.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- eslint-disable vue/no-v-model-argument -->
+  <v-row>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <OTAOptions
+        v-model:isIncremental="isIncremental"
+        :targetDetails="targetDetails"
+        :incrementalSource="incrementalSource"
+        :targetBuild="targetBuild"
+      />
+    </v-col>
+    <v-divider vertical />
+    <v-col
+      cols="12"
+      md="6"
+      class="library"
+    >
+      <BuildLibrary
+        v-model:incrementalSource="incrementalSource"
+        v-model:targetBuild="targetBuild"
+        v-model:targetDetails="targetDetails"
+        :isIncremental="isIncremental"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import OTAOptions from '@/components/OTAOptions.vue'
+import BuildLibrary from '@/components/BuildLibrary.vue'
+
+export default {
+  components: {
+    OTAOptions,
+    BuildLibrary
+  },
+  data() {
+    return {
+      incrementalSource: '',
+      targetBuild: '',
+      targetDetails: [],
+      isIncremental: false
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .library {
+    overflow: scroll;
+    height:calc(100vh - 80px);
+  }
+</style>>
