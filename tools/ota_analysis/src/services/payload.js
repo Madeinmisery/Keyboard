60422e4653f8d1d7f9cda6da5f6a217edf38f807
Kelvin Zhang <zhangkelvin@google.com>
2021-07-26 19:40:00 -0400

    Prototype for reading only the manifest part of payload
    Test: open a large OTA package
    Change-Id: Iebf8045325dae9a118d9d8ea5674872aa7c280c4

--- tools/ota_analysis/src/services/payload.js
+++ tools/ota_analysis/src/services/payload.js
@@ -10,11 +10,98 @@
 import * as zip from '@zip.js/zip.js/dist/zip-full.min.js'
 import { chromeos_update_engine as update_metadata_pb } from './update_metadata_pb.js'
 
-const /** Number */ _MAGIC = 'CrAU'
+const /** string */ _MAGIC = 'CrAU'
 const /** Number */ _VERSION_SIZE = 8
 const /** Number */ _MANIFEST_LEN_SIZE = 8
 const /** Number */ _METADATA_SIGNATURE_LEN_SIZE = 4
+
+const /** @type {Number} */ _PAYLOAD_HEADER_SIZE = _MAGIC.length + _VERSION_SIZE + _MANIFEST_LEN_SIZE + _METADATA_SIGNATURE_LEN_SIZE
+
 const /** Number */ _BRILLO_MAJOR_PAYLOAD_VERSION = 2
+
+class StopIteration extends Error {
+
+}
+
+function readIntAtOffset(buffer, cursor, size) {
+  let /** DataView */ view = new DataView(
+    buffer.slice(cursor, cursor + size))
+  cursor += size
+  switch (size) {
+  case 2:
+    return view.getUInt16(0)
+  case 4:
+    return view.getUint32(0)
+  case 8:
+    return Number(view.getBigUint64(0))
+  default:
+    throw 'Cannot read this integer with size ' + size
+  }
+}
+
+async function parsePayloadHeader(/** @type {Blob} */buffer) {
+  buffer = await buffer.slice(0, _PAYLOAD_HEADER_SIZE).arrayBuffer();
+  let /** TextDecoder */ decoder = new TextDecoder()
+  let cursor = 0;
+  let magic = decoder.decode(
+    buffer.slice(0, _MAGIC.length));
+  if (magic != _MAGIC) {
+    throw new Error('MAGIC is not correct, please double check.');
+  }
+  cursor += _MAGIC.length;
+  const readInt = size => {
+    const ret = readIntAtOffset(buffer, cursor, size);
+    cursor += size;
+    return ret;
+  }
+  let header_version = readInt(_VERSION_SIZE);
+  let manifest_len = readInt(_MANIFEST_LEN_SIZE);
+
+  if (header_version != _BRILLO_MAJOR_PAYLOAD_VERSION) {
+    throw new Error("Unexpected major version number: " + header_version)
+  }
+  let metadata_signature_len = readInt(_METADATA_SIGNATURE_LEN_SIZE)
+  return { magic, header_version, manifest_len, metadata_signature_len };
+}
+
+class OTAPayloadBlobWriter extends zip.Writer {
+
+  constructor(contentType = "") {
+    super();
+    this.offset = 0;
+    this.contentType = contentType;
+    this.blob = new Blob([], { type: contentType });
+    this.prefixLength = 0;
+  }
+
+  async writeUint8Array(/**  @type {Uint8Array} */ array) {
+    // if (this.prefixLength > 0) {
+    //   console.log(`Trimming end of array`);
+    //   if (array.length + this.offset > this.prefixLength) {
+    //     array = array.slice(0, this.prefixLength - this.offset);
+    //   }
+    // }
+    console.log(array.length);
+    super.writeUint8Array(array);
+
+    this.blob = new Blob([this.blob, array.buffer], { type: this.contentType });
+    this.offset = this.blob.size;
+    if (this.prefixLength > 0) {
+      console.log(`${this.offset / this.prefixLength}% done`)
+    } else if (this.offset >= _PAYLOAD_HEADER_SIZE) {
+      const header = await parsePayloadHeader(this.blob);
+      this.prefixLength = _PAYLOAD_HEADER_SIZE + header.manifest_len + header.metadata_signature_len;
+      console.log(`Metadata size(including header and manifest size) is ${this.prefixLength}`)
+    }
+    if (this.offset >= this.prefixLength) {
+      throw new StopIteration();
+    }
+  }
+
+  getData() {
+    return this.blob;
+  }
+}
 
 export class Payload {
   /**
@@ -31,15 +118,27 @@ export class Payload {
     this.payload = null
     for (let entry of entries) {
       if (entry.filename == 'payload.bin') {
-        //TODO: only read in the manifest instead of the whole payload
-        this.payload = await entry.getData(new zip.BlobWriter())
+        let writer = new OTAPayloadBlobWriter("");
+        try {
+          await entry.getData(writer);
+        } catch (e) {
+          if (e instanceof StopIteration) {
+            // Exception used as a hack to stop reading from zip. NO need to do anything
+            // Ideally zip.js would provide an API to partialll read a zip
+            // entry, but they don't. So this is what we get
+          } else {
+            throw e;
+          }
+        }
+        this.payload = writer.getData();
+        break;
       }
     }
     if (!this.payload) {
       alert('Please select a legit OTA package')
       return
     }
-    this.buffer = await this.payload.arrayBuffer()
+    this.buffer = await this.payload.arrayBuffer();
   }
 
   /**
@@ -149,7 +248,7 @@ export class MergeOpType {
 export function octToHex(bufferArray, space = true, maxLine = 16) {
   let hex_table = ''
   for (let i = 0; i < bufferArray.length; i++) {
-    if (bufferArray[i].toString(16).length===2) {
+    if (bufferArray[i].toString(16).length === 2) {
       hex_table += bufferArray[i].toString(16) + (space ? ' ' : '')
     } else {
       hex_table += '0' + bufferArray[i].toString(16) + (space ? ' ' : '')
