b9f80598dc28a20550a1484ea0c053314900aeb7
Inseob Kim <inseob@google.com>
2021-09-24 14:21:06 +0900

    Add inseob@ to OWNERS
    Test: N/A
    Change-Id: I2f7f739b53312e7c99f3a3a28cf164eb20582a34

--- vndk/snapshot/update.py
+++ vndk/snapshot/update.py
@@ -94,7 +94,14 @@ def install_snapshot(branch, build, local_dir, install_dir, temp_artifact_dir):
     for artifact in artifacts:
         logging.info('Unzipping VNDK snapshot: {}'.format(artifact))
         utils.check_call(['unzip', '-qn', artifact, '-d', install_dir])
-
+        # rename {install_dir}/{arch}/include/out/soong/.intermediates
+        for soong_intermediates_dir in glob.glob(install_dir + '/*/include/' + utils.SOONG_INTERMEDIATES_DIR):
+            generated_headers_dir = soong_intermediates_dir.replace(
+                utils.SOONG_INTERMEDIATES_DIR,
+                utils.GENERATED_HEADERS_DIR
+            )
+            logging.debug('Renaming {} to {}'.format(soong_intermediates_dir, generated_headers_dir))
+            os.rename(soong_intermediates_dir, generated_headers_dir)
 
 def gather_notice_files(install_dir):
     """Gathers all NOTICE files to a common NOTICE_FILES directory."""
