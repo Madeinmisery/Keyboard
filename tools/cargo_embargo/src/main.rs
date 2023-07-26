de7fbb9e4c4a807710d69d3e9f524f055d3692c6
Andrew Walbran <qwandor@google.com>
2023-07-26 16:30:07 +0100

    Allow default PackageConfig to be specified in top-level config.
    This allows simpler configuration for single-package configurations
    and matches the behaviour of cargo2android.py.
    
    Bug: 293289578
    Test: Ran on external/rust/crates/either
    Change-Id: I88e8454acf51d2d65c59b201e55a2ec462d88d1d

--- tools/cargo_embargo/src/main.rs
+++ tools/cargo_embargo/src/main.rs
@@ -235,8 +235,7 @@ fn write_android_bp(
     let bp_path = package_dir.join("Android.bp");
 
     let package_name = crates[0].package_name.clone();
-    let def = PackageConfig::default();
-    let package_cfg = cfg.package.get(&package_name).unwrap_or(&def);
+    let package_cfg = cfg.package.get(&package_name).unwrap_or(&cfg.default_package);
 
     // Keep the old license header.
     let license_section = match std::fs::read_to_string(&bp_path) {
