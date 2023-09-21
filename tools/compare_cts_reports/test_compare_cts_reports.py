c79a9d6049f9f96400d16f44594e404d963b7e6c
Zoe Tsou <zoetsou@google.com>
2023-09-21 12:52:45 +0000

    Add unit tests for tools/compare_cts_reports/compare_cts_reports.py.
    Bug: 292453652
    Test: ./test_compare_cts_reports.py
    Change-Id: I859553dbbbdf9adeac6bb96b227073d0a3aa2c9b

---
+++ tools/compare_cts_reports/test_compare_cts_reports.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+import os
+import tempfile
+import filecmp
+import unittest
+import compare_cts_reports
+import parse_cts_report
+
+
+class TestParse(unittest.TestCase):
+
+  def test_one_way(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      ctsreports = [
+          parse_cts_report.parse_report_file('testdata/test_result_1.xml'),
+          parse_cts_report.parse_report_file('testdata/test_result_2.xml'),
+      ]
+      csvfile = os.path.join(temp_dir, 'one_way_diff.csv')
+      compare_cts_reports.one_way_compare(ctsreports, csvfile)
+
+      self.assertEqual(filecmp.cmp('testdata/compare/one_way_diff.csv', csvfile), True)
+
+  def test_two_way(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      ctsreports = [
+          parse_cts_report.parse_report_file('testdata/test_result_1.xml'),
+          parse_cts_report.parse_report_file('testdata/test_result_2.xml'),
+      ]
+      csvfile = os.path.join(temp_dir, 'two_way_diff.csv')
+      compare_cts_reports.two_way_compare(ctsreports, csvfile)
+
+      self.assertEqual(filecmp.cmp('testdata/compare/two_way_diff.csv', csvfile), True)
+
+  def test_n_way(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      ctsreports = [
+          parse_cts_report.parse_report_file('testdata/test_result_1.xml'),
+          parse_cts_report.parse_report_file('testdata/test_result_2.xml'),
+      ]
+      csvfile = os.path.join(temp_dir, 'n_way_diff.csv')
+      compare_cts_reports.n_way_compare(ctsreports, csvfile)
+
+      self.assertEqual(filecmp.cmp('testdata/compare/n_way_diff.csv', csvfile), True)
+
+if __name__ == '__main__':
+  unittest.main()
--- tools/compare_cts_reports/test_parse_cts_report.py
+++ tools/compare_cts_reports/test_parse_cts_report.py
@@ -140,15 +140,16 @@ class TestParse(unittest.TestCase):
       parsed_result_path = os.path.join(temp_dir, 'result.csv')
       parsed_summary_path = os.path.join(temp_dir, 'summary.csv')
 
-      files = [parsed_info_path, parsed_result_path, parsed_summary_path]
-
-      match, mismatch, errors = filecmp.cmpfiles(
-          'testdata/output', temp_dir, files
+      self.assertEqual(
+          filecmp.cmp('testdata/output/info_1.json', parsed_info_path), True
       )
-
-      self.assertEqual(match, files)
-      self.assertEqual(mismatch, [])
-      self.assertEqual(errors, [])
+      self.assertEqual(
+          filecmp.cmp('testdata/output/result_1.csv', parsed_result_path), True
+      )
+      self.assertEqual(
+          filecmp.cmp('testdata/output/summary_1.csv', parsed_summary_path),
+          True,
+      )
 
 
 if __name__ == '__main__':
