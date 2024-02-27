3b716425032b6ef3852d78cc617ff82344ebf00a
Hsin-Yi Chen <hsinyichen@google.com>
2024-02-27 13:04:51 +0800

    Omit self_type and referenced_type
    For TypeIR and GlobalVarIR, the JSON dumper omits self_type if it's
    equal to linker_set_key; omits referenced_type if it's equal to
    self_type.
    
    Bug: 323447559
    Test: ANDROID_BUILD_TOP=`realpath .` \
          PATH=$PATH:`realpath out/soong/dist/bin` \
          development/vndk/tools/header-checker/tests/test.py
    Change-Id: Id1e46046fa60fe6c3bb054461fa78f8beda500bb

--- vndk/tools/header-checker/src/repr/json/ir_reader.cpp
+++ vndk/tools/header-checker/src/repr/json/ir_reader.cpp
@@ -207,13 +207,20 @@ void JsonIRReader::ReadTemplateInfo(const JsonObjectRef &type_decl,
 
 void JsonIRReader::ReadTypeInfo(const JsonObjectRef &type_decl,
                                 TypeIR *type_ir) {
-  type_ir->SetLinkerSetKey(type_decl.GetString("linker_set_key"));
+  // LinkableMessageIR
   type_ir->SetSourceFile(type_decl.GetString("source_file"));
+  type_ir->SetLinkerSetKey(type_decl.GetString("linker_set_key"));
+  // TypeIR
   type_ir->SetName(type_decl.GetString("name"));
-  type_ir->SetReferencedType(type_decl.GetString("referenced_type"));
-  type_ir->SetSelfType(type_decl.GetString("self_type"));
   type_ir->SetSize(type_decl.GetUint("size"));
   type_ir->SetAlignment(type_decl.GetUint("alignment"));
+  std::string self_type = type_decl.GetString("self_type");
+  type_ir->SetSelfType(self_type.size() > 0 ? self_type
+                                            : type_ir->GetLinkerSetKey());
+  // ReferencesOtherType
+  std::string referenced_type = type_decl.GetString("referenced_type");
+  type_ir->SetReferencedType(
+      referenced_type.size() > 0 ? referenced_type : type_ir->GetSelfType());
 }
 
 void JsonIRReader::ReadRecordFields(const JsonObjectRef &record_type,
@@ -319,13 +326,18 @@ EnumTypeIR JsonIRReader::EnumTypeJsonToIR(const JsonObjectRef &enum_type) {
 void JsonIRReader::ReadGlobalVariables(const JsonObjectRef &tu) {
   for (auto &&global_variable : tu.GetObjects("global_vars")) {
     GlobalVarIR global_variable_ir;
+    // GlobalVarIR
     global_variable_ir.SetName(global_variable.GetString("name"));
     global_variable_ir.SetAccess(GetAccess(global_variable));
+    // LinkableMessageIR
     global_variable_ir.SetSourceFile(global_variable.GetString("source_file"));
-    global_variable_ir.SetReferencedType(
-        global_variable.GetString("referenced_type"));
     global_variable_ir.SetLinkerSetKey(
         global_variable.GetString("linker_set_key"));
+    // ReferencesOtherType
+    std::string referenced_type = global_variable.GetString("referenced_type");
+    global_variable_ir.SetReferencedType(
+        referenced_type.size() > 0 ? referenced_type
+                                   : global_variable_ir.GetLinkerSetKey());
     module_->AddGlobalVariable(std::move(global_variable_ir));
   }
 }
